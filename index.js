17fb500a1006b8f7a705451a6523a287fd279809
pvalette <pvalette06@gmail.com>
2018-11-11 12:33:59 +0100

    first commit

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 
 function main()
 {
-	console.log("In main function")
+	console.log("In main function");
 }
 function main2()
 {
-	console.log("In main2 function")
+	console.log("In main2 function");
 }
 
 function main3()
 {
-	console.log("In main2 function")
+	console.log("In main2 function");
 }
 
 function error()
@@ -25,5 +25,5 @@ function error()
 
 main();
 main2();
-main3();
-console.log(error())
+if (0) main3();
+console.log(error());
